152f7f1b6310bad2cce2c508760f55df5704634d
qhuy2412 <doquanghuy24122003@gmail.com>
2026-03-19 15:51:34 +0700

    fix: reconfig cors

--- backend/server.js
+++ backend/server.js
@@ -17,24 +17,16 @@ const dashboardRoute = require("./router/dashboardRoute");
 const db = require("./config/db");
 const app = express();
 
-// Config CORS
-const corsOptions = {
-  origin: 'http://localhost:5173', 
-  
-  credentials: true,
-  
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
 
-app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: true, // Cho phép tất cả các nguồn để test trước
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 app.use("/api/auth", authRoute);
 app.use("/api/wallets", walletRoute);

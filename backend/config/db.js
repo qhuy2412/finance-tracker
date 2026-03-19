4515651dbb36e89ed8fd0600e600587d1c2dfbb6
qhuy2412 <doquanghuy24122003@gmail.com>
2026-03-19 14:31:15 +0700

    refactor: refactor db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,15 @@ require("dotenv").config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    uri : process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT) || 4000,
+    ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true,
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0

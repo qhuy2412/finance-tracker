527af97f3cea1b9a11b1fe27c45c9505b5b58e6a
qhuy2412 <doquanghuy24122003@gmail.com>
2026-03-26 14:15:52 +0700

    refactor: config database for vm

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,11 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT) || 4000,
-    ssl: {
+    port: parseInt(process.env.DB_PORT) || 3306,
+    ssl: process.env.DB_SSL === 'true' ? {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true,
-    },
+    } : null,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
